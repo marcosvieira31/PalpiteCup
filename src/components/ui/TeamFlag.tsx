4b8ec91d7fd32ac40df0bc3acc2cedf2352a8673
Marcos Vieira <marcosnd.31@gmail.com>
2026-06-05 21:03:17 -0300

    debug: add unoptimized and log flag urls

--- src/components/ui/TeamFlag.tsx
+++ src/components/ui/TeamFlag.tsx
@@ -15,7 +15,8 @@ export default function TeamFlag({ team, size = 40, className = '' }: Props) {
   useEffect(() => {
     setMounted(true)
     setImgError(false)
-  }, [team])
+    console.log('FLAG URL:', getFlagUrl(team, size))
+  }, [team, size])
 
   const flagUrl = getFlagUrl(team, size)
 
