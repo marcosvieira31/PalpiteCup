7ac8858550f3aa32f0c08dae80316038a871c8a8
Marcos Vieira <marcosnd.31@gmail.com>
2026-06-10 01:24:48 -0300

    feat: adiciona 9 novas figurinhas de jogadores

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -12,6 +12,15 @@ export const PLAYER_AVATARS: Avatar[] = [
   { id: 'mbappe', name: 'Mbappé', file: '/avatars/mbappe.png' },
   { id: 'haaland', name: 'Haaland', file: '/avatars/haaland.png' },
   { id: 'cristiano-ronaldo', name: 'Cristiano Ronaldo', file: '/avatars/cristiano-ronaldo.png' },
+  { id: 'bellingham', name: 'Bellingham', file: '/avatars/bellingham.png' },
+  { id: 'lamine-yamal', name: 'Lamine Yamal', file: '/avatars/lamine-yamal.png' },
+  { id: 'manuel-neuer', name: 'Manuel Neuer', file: '/avatars/manuel-neuer.png' },
+  { id: 'salah', name: 'Salah', file: '/avatars/salah.png' },
+  { id: 'sadio-mane', name: 'Sadio Mané', file: '/avatars/sadio-mane.png' },
+  { id: 'hirving-lozano', name: 'Hirving Lozano', file: '/avatars/hirving-lozano.png' },
+  { id: 'memphis-depay', name: 'Memphis Depay', file: '/avatars/memphis-depay.png' },
+  { id: 'mitoma', name: 'Mitoma', file: '/avatars/mitoma.png' },
+  { id: 'lucas-paqueta', name: 'Lucas Paquetá', file: '/avatars/lucas-paqueta.png' },
 ]
 
 export const DEFAULT_AVATAR = '/avatars/vini-jr.png'
